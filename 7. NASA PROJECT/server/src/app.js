dd7fb6d76f05ae8384395fa8e80a7ed8b5978665
Dev Sen <devksen.personal@gmail.com>
2025-10-22 18:16:53 +0530

    feat: serving React.js in production & add morgan middleware to log the information of client's requests

--- 7. NASA PROJECT/server/src/app.js
+++ 7. NASA PROJECT/server/src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');    // Import Express
 const cors = require('cors')           //Import Cors
+const path = require('path')
+const morgan = require('morgan')
+
 
 const planetsRouter = require('./routes/planets/planets.router')
 const app = express();                  // Create Express app
@@ -7,7 +10,15 @@ const app = express();                  // Create Express app
 app.use(cors({
  origin: 'http://localhost:3000',        //Add Cors Middleware
 }))
+
+app.use(morgan('combined'))
+
 app.use(express.json());                // Middleware to parse JSON request bodies
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(planetsRouter)
+app.get('/', (req, res) => {
+ res.sendFile(path.join(__dirname, '..', 'public'))
+})
+
 
 module.exports = app;                   // Export app for use in server.js
