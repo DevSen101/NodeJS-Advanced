addb66ea8eecaaa0c9a16c171d30c328cde3fa5d
Dev Sen <devksen.personal@gmail.com>
2025-10-25 14:41:00 +0530

    learn pm2 tool to manage load balancing and make child proccess

--- 9. IMPROVING NODE PERFORMANCE/2. PERFORMANCE IMPROVEMENT/server.js
+++ 9. IMPROVING NODE PERFORMANCE/2. PERFORMANCE IMPROVEMENT/server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const cluster = require('cluster');
-const os = require('os');
 
 const app = express();
 
@@ -23,14 +21,6 @@ app.get('/timer', (req, res) => {
   res.send(`Ding ding ding! ${process.pid}`);
 });
 
-console.log('Running server.js...');
-if (cluster.isMaster) {
-  console.log('Master has been started...');
-  const NUM_WORKERS = os.cpus().length;
-  for (let i = 0; i < NUM_WORKERS; i++) {
-    cluster.fork();
-  }
-} else {
+  console.log('Running server.js...');
   console.log('Worker process started.');
   app.listen(3000);
-}
