4785cca4d92547763c6280ae806d69d03c212527
Dev Sen <devksen.personal@gmail.com>
2025-10-28 00:00:57 +0530

    fetch spacex api by axios and filter fetched data

--- 7. NASA PROJECT/server/src/server.js
+++ 7. NASA PROJECT/server/src/server.js
@@ -1,6 +1,7 @@
 const http = require('http');           // Import Node HTTP module
 const app = require('./app');           // Import Express app
 const  { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model')
 const { mongoConnect } = require('./services/mongo');
 
 const PORT = process.env.PORT || 8000;  // Set 
@@ -12,6 +13,7 @@ const server = http.createServer(app);  // Create HTTP server with Express app
 async function startServer(){ 
  await mongoConnect()
  await loadPlanetsData()
+ await loadLaunchData()
 
 server.listen(PORT, () => console.log(`✅ Server listening on PORT ${PORT}...`)); // Start server
 }
