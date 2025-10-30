095655676f9ff4ce2178c3d94e05596dbd957fac
Dev Sen <devksen.personal@gmail.com>
2025-10-30 17:46:12 +0530

    Add middleware that fakes req.session.regenerate and req.session.save for cookie-session

--- 10. NODE SECURITY & AUTHENTICATION/4. SECURITY EXAMPLE/server.js
+++ 10. NODE SECURITY & AUTHENTICATION/4. SECURITY EXAMPLE/server.js
@@ -56,6 +56,23 @@ app.use(cookieSession({
 
 }))
 
+app.use((req, res, next) => {
+  // Stub out missing regenerate and save functions.
+  // These don't make sense for client side sessions.
+  if (req.session && !req.session.regenerate) {
+    req.session.regenerate = (cb) => {
+      cb();
+    };
+  }
+  if (req.session && !req.session.save) {
+    req.session.save = (cb) => {
+      cb();
+    };
+  }
+  next();
+});
+
+
 app.use(passport.initialize()) // Initialize Passport middleware
 app.use(passport.session())
 
