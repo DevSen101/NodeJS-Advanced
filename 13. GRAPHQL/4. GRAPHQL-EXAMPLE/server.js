d38dd87464da9e7dfee1fe9a109a8d52d4c950ec
Dev Sen <devksen.personal@gmail.com>
2025-11-03 21:12:49 +0530

    Create a sample E-commerce Graphql server

--- 13. GRAPHQL/4. GRAPHQL-EXAMPLE/server.js
+++ 13. GRAPHQL/4. GRAPHQL-EXAMPLE/server.js
@@ -6,15 +6,64 @@ const { graphqlHTTP } = require('express-graphql')
 // Define GraphQL schema with Query type
 const schema = buildSchema(`
  type Query {
-    description: String
-    price: Float
+    products: [Product]
+    orders: [Order]
  }
+    type Product {
+     id: ID!
+     description: String!
+     reviews: [Review]
+     price: Float!
+    }
+
+    type Review {
+     rating: Int!
+     comment: String
+    }
+
+    type Order {
+     date: String!
+     subtotal: Float!
+     items: [OrderItem]
+    }
+
+    type OrderItem {
+     product: Product!
+     quantity: Int!
+    }
  `)
 
 // Define root resolver (provides data for schema fields)
 const root = {
-  description: 'Red Shoe',
-  price: 42.12,
+   products: [
+    {
+      id: 'redshoe',
+      description: 'Red Shoe',
+      price: 50.32
+    },
+
+    {
+      id: 'bluejean',
+      description: 'Blue Jean',
+      price:55.46
+    }
+   ],
+   orders: [
+    {
+      date: '2025-05-05',
+      subtotal: 90.56,
+      items: [
+        {
+          product: {
+            id: 'redshoe',
+            description: 'Red Shoe',
+            price: 50.32
+          },
+          quantity: 2
+        }
+      ]
+    }
+   ]
 }
 
 // Initialize Express app
