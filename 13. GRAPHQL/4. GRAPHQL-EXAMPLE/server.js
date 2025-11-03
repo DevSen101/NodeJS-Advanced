3939c88c62cf1e22204d62a0cc02b261a511c4d9
Dev Sen <devksen.personal@gmail.com>
2025-11-04 00:58:14 +0530

    Replace express-graphql with apollo-server

--- 13. GRAPHQL/4. GRAPHQL-EXAMPLE/server.js
+++ 13. GRAPHQL/4. GRAPHQL-EXAMPLE/server.js
@@ -2,7 +2,7 @@
 const path = require('path')
 const express = require('express')
 
-const { graphqlHTTP } = require('express-graphql')
+const { ApolloServer } = require('apollo-server-express')
 const { loadFilesSync } = require('@graphql-tools/load-files')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
 
@@ -10,25 +10,36 @@ const { makeExecutableSchema } = require('@graphql-tools/schema')
 const typesArray = loadFilesSync('**/*', {
 extensions: ['.graphql']  
 })
-
 const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'))
+
+async function startApolloServer() {
+ // Initialize Express app
+const app = express()
 
 // Define GraphQL schema with Query type
 const schema = makeExecutableSchema({
   typeDefs: typesArray,
   resolvers: resolversArray
-}) 
+})
 
-// Initialize Express app
-const app = express()
+const server = new ApolloServer({
+  schema
+})
 
-// Setup GraphQL endpoint middleware
-app.use('/graphql', graphqlHTTP({
- schema: schema,     // Attach schema
- graphiql: true   // (Optional) Enable GraphiQL UI
-}))
+await server.start();
+server.applyMiddleware({ app, path: '/graphql'})
 
 // Start server on port 3000
 app.listen(3000, () => {
  console.log('GraphQL Server Running...')
-})
+ })
+}
+
+startApolloServer();
+
+ 
+
+
+
+
+
